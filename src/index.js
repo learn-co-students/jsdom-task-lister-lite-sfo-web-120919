d18d839399760516fa25b00d5073b60805eec6c5
coronaisme <coronaisme@gmail.com>
2020-01-28 14:15:18 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //functions
+  formSubmit();
+  clickToDelete();
 });
+
+//add to tasks
+const formSubmit = () => {
+  const form = document.getElementById('create-task-form');
+  
+  form.addEventListener("submit", handleSubmit);
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  
+  const input = e.target.querySelector('#new-task-description')
+  const todos = document.getElementById('tasks')
+  
+  const tasks = document.createElement('p');
+  tasks.textContent = input.value
+  
+  todos.appendChild(tasks)
+  
+  input.value = ''
+}
+
+
+//delete
+const clickToDelete = () => {
+  const tasks = document.getElementById('tasks');
+  tasks.addEventListener("click", handleClick)
+}
+
+const handleClick = (e) => {
+  const tasks = document.getElementById('tasks');
+  tasks.removeChild(e.target)
+
+}
