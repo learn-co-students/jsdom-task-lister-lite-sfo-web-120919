1033e19b8d8be81dd7d49bfbd38004e6f0ae5d16
github username <github email address>
2020-01-28 15:58:33 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  listenToTaskSubmit();
 });
+
+
+function listenToTaskSubmit() {
+  const task = document.getElementById("create-task-form");
+
+  task.addEventListener("submit", handleTaskSubmit);
+}
+
+function handleTaskSubmit(event) {
+  event.preventDefault();
+
+  const inputField = event.target.querySelector("#new-task-description");
+  const taskBox = document.getElementById("tasks");
+
+  const createLi = document.createElement("li");
+  createLi.textContent = inputField.value;
+  taskBox.appendChild(createLi);
+  
+  const createButton = document.createElement("button")
+  createButton.textContent = "X"
+  createButton.setAttribute("data-name", inputField.value);
+  createLi.appendChild(createButton);
+
+  // commentsContainer.innerHTML += `<h2>hello there</h2>`;
+
+  inputField.value = "";
+
+  taskBox.addEventListener('click', handleClick)
+
+
+  function removeTask(button) {
+      // remove task here
+
+
+
+      
+      const parent = button.parentElement
+   
+      
+      
+       console.log(parent.tagName)
+       if (parent.tagName === "LI") {
+         console.log("triggered")
+         parent.remove();
+       }
+  }
+
+  function handleClick(e) {
+
+     
+    removeTask(e.target)
+
+
+  }
+
+}
+
