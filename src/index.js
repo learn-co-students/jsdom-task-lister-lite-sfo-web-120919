c9ed0e70e312b93b470bc01654ee3467c3cb849a
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-01-28 16:22:15 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   // your code here
   gettask();
+  sele();
 });
 
 function gettask()
@@ -16,8 +17,11 @@ function getvalue(event)
   event.preventDefault();
   const c = document.getElementById("new-task-description");
   const b = document.getElementById("tasks");
+  const se = document.getElementById("select");
+
   const ele = document.createElement('li');
-  ele.textContent = c.value
+  // ele.textContent = c.value
+  ele.innerHTML = c.value.fontcolor(se.value)
   b.appendChild(ele);
   
   
@@ -27,7 +31,7 @@ function getvalue(event)
   ele.appendChild(but)
 
   b.addEventListener("click",func);
-
+  
   c.value = "";
   
 }
@@ -38,4 +42,29 @@ function func(event)
   const pa = t.parentElement;
   pa.remove();
   // debugger;
+}
+
+function sele()
+{
+  const s = document.createElement("select");
+  s.id = "select"
+
+  const so1 = document.createElement("option");
+  so1.id = "red";
+  so1.value = "red";
+  so1.textContent= "red";
+  const so2 = document.createElement("option");
+  so2.id = "yellow";
+  so2.value = "yellow";
+  so2.textContent= "yellow";
+  const so3 = document.createElement("option");
+  so3.id = "green";
+  so3.value = "green";
+  so3.textContent= "green";
+
+  const f = document.getElementById("create-task-form");
+  f.appendChild(s);
+  s.appendChild(so1);
+  s.appendChild(so2);
+  s.appendChild(so3);
 }
